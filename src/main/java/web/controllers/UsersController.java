56a8fadca50cb042410e698886792c924e3a0b6f
gdv24 <dgolovin@ngs.ru>
2021-11-18 12:23:14 +0300

    done T3_1_2

--- src/main/java/web/controllers/UsersController.java
+++ src/main/java/web/controllers/UsersController.java
@@ -7,12 +7,10 @@ import org.springframework.web.bind.annotation.*;
 import web.dao.RoleDaoImpl;
 import web.dao.UserDaoImpl;
 import web.model.User;
-import web.model.Role;
 
 
 import java.security.Principal;
 import java.util.ArrayList;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/")
@@ -37,30 +35,12 @@ public class UsersController {
         return "index";
     }
 
-    @GetMapping("/admin/{id}")
-    public String show(@PathVariable("id") Long id, Model model, Principal principal){
-        model.addAttribute("users",userDaoImpl.show(id));
-        return "show";
-    }
-
-    @GetMapping("/new")
-    public String newUser(@ModelAttribute("user") User user, Model model){
-        model.addAttribute("role",roleDaoImpl.getAllRoles());
-        return "new";
-    }
-
     @PostMapping("/adduser")
     public String create(@ModelAttribute("user") User user,
                          @RequestParam("role") ArrayList<Long> role){
         user.setRoles(roleDaoImpl.getByIdRoles(role));
         userDaoImpl.save(user);
         return "redirect:/admin";
-    }
-
-    @GetMapping("/{id}/edit")
-    public String edit(Model model, @PathVariable("id") Long id) {
-        model.addAttribute("users",userDaoImpl.show(id));
-        return "edit";
     }
 
     @PostMapping("/{id}")
@@ -71,14 +51,6 @@ public class UsersController {
         return "redirect:/admin";
     }
 
-
-    @PatchMapping("/update/{id}")
-    public String update(@ModelAttribute("user") User user, @RequestParam(required=false, name = "role") ArrayList<Long> role){
-//                         @PathVariable("id") Long id){
-//        user.setRoles(roleDaoImpl.getByIdRoles(role));
-        userDaoImpl.update(user);
-        return "redirect:/admin";
-    }
     @DeleteMapping("/{id}")
     public String delete(@PathVariable("id") Long id){
         userDaoImpl.delete(id);
